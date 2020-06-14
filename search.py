bd65134ab3841aff69952ae2e876d945d2c5c3fb
bnpysse <bnpysse@aliyun.com>
2020-06-14 10:07:44 +0800

    再次对Summary中的双引号、单引号处理，以免在写入数据库时发生错误。2020-06-14 10:07:31

--- search.py
+++ search.py
@@ -3,14 +3,14 @@ import re
 import sqlite3
 import urllib.parse
 from datetime import datetime
-
+import pprint
 import requests
 from pyquery import PyQuery as pq
 
 login_cookies_dict = {
     '_ga': 'GA1.2.576373095.1589077068',
     # 只需要改这个东东就可以，2020-06-11 17:14:42
-    'ezproxy': 'vx3YNxUs76mX7xT',
+    'ezproxy': 'BosUt2d5p0TWQrw',
 }
 
 login_url = 'http://data.people.com.cn.proxy.library.georgetown.edu/rmrb/20200515/1?code=2'
@@ -59,24 +59,24 @@ qs = {"cIds": "23,", "cds": [
                      {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "AND", "val": "暴力"},
                      {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "AND", "val": "暴力"}]}]}]}],
       "obs": [{"fld": "dataTime", "drt": "DESC"}, {"fld": "orderId", "drt": "DESC"}, {"fld": "seqid", "drt": "DESC"}]}
+
+
 # endregion
 
 # region 测试查询字典的关键字位置
-print(urllib.parse.urlencode(qs))
-print(str(qs).replace('\'', '\"'))
-print(urllib.parse.quote(str(qs).replace('\'', '\"')))
-# 第一个关键字
-print('{}'.format(qs['cds'][1]['cds'][0]['cds'][0]['cds'][0]['val']))  # title
-print('{}'.format(qs['cds'][1]['cds'][0]['cds'][0]['cds'][1]['val']))  # subTitle
-print('{}'.format(qs['cds'][1]['cds'][0]['cds'][0]['cds'][2]['val']))  # introTitle
-print('{}'.format(qs['cds'][1]['cds'][0]['cds'][0]['cds'][3]['val']))  # contentText
-# 第二个关键字
-print('{}'.format(qs['cds'][1]['cds'][0]['cds'][1]['cds'][0]['val']))
-print('{}'.format(qs['cds'][1]['cds'][0]['cds'][1]['cds'][1]['val']))
-print('{}'.format(qs['cds'][1]['cds'][0]['cds'][1]['cds'][2]['val']))
-print('{}'.format(qs['cds'][1]['cds'][0]['cds'][1]['cds'][3]['val']))
-
-
+# print(urllib.parse.urlencode(qs))
+# print(str(qs).replace('\'', '\"'))
+# print(urllib.parse.quote(str(qs).replace('\'', '\"')))
+# # 第一个关键字
+# print('{}'.format(qs['cds'][1]['cds'][0]['cds'][0]['cds'][0]['val']))  # title
+# print('{}'.format(qs['cds'][1]['cds'][0]['cds'][0]['cds'][1]['val']))  # subTitle
+# print('{}'.format(qs['cds'][1]['cds'][0]['cds'][0]['cds'][2]['val']))  # introTitle
+# print('{}'.format(qs['cds'][1]['cds'][0]['cds'][0]['cds'][3]['val']))  # contentText
+# # 第二个关键字
+# print('{}'.format(qs['cds'][1]['cds'][0]['cds'][1]['cds'][0]['val']))
+# print('{}'.format(qs['cds'][1]['cds'][0]['cds'][1]['cds'][1]['val']))
+# print('{}'.format(qs['cds'][1]['cds'][0]['cds'][1]['cds'][2]['val']))
+# print('{}'.format(qs['cds'][1]['cds'][0]['cds'][1]['cds'][3]['val']))
 # endregion
 
 
@@ -92,8 +92,8 @@ def building_qs(word_list1, word_list2):
 
 def get_word_frequency(word_list1, word_list2, table_name):
     count = 0
-    create_sqlite_db(database_name)
-    insert_sql = 'insert into `{}`(Title, Date, Keywords, Count, Summary) values("{}","{}","{}","{}","{}","{}")'
+    create_sqlite_db(table_name)
+    insert_sql = 'insert into `{}`(Title, Date, Keywords, Count, Summary) values("{}","{}","{}",{},"{}")'
     conn = sqlite3.connect(database_name)
     cursor = conn.cursor()
     start_time = datetime.now()
@@ -103,7 +103,7 @@ def get_word_frequency(word_list1, word_list2, table_name):
             keywords, url = next(gen)
             # 首先取出第1页的内容，然后获得总的页数，再取剩余的页的内容
             form_data = {'pageNo': 1, 'pageSize': pageSize}
-            response = login_s.post(url, date=form_data, headers=post_headers, cookies=cookies).content.decode('utf-8')
+            response = login_s.post(url, data=form_data, headers=post_headers, cookies=cookies).content.decode('utf-8')
             doc = pq(response)
             counter_string = doc('.pagination').text()
             rec = re.findall('共( \d+ )条', counter_string)
@@ -113,7 +113,7 @@ def get_word_frequency(word_list1, word_list2, table_name):
                 start_page_time = datetime.now()
                 form_data['pageNo'] = page
                 content_dict = [dict() for i in range(pageSize)]
-                response = login_s.post(url, date=form_data, headers=post_headers, cookies=cookies).content.decode(
+                response = login_s.post(url, data=form_data, headers=post_headers, cookies=cookies).content.decode(
                     'utf-8')
                 doc = pq(response)
                 for k, v in enumerate(doc('.articleSum_li').items()):
@@ -124,24 +124,32 @@ def get_word_frequency(word_list1, word_list2, table_name):
                     lines = v.text().split('\n')
                     content_dict[k]['Date'] = lines.pop(-1)[-10:]
                     content_dict[k]['Summary'] = re.sub('<a href.*', '',
-                                                        v('p').html().replace('\t', '').replace('\n', ''))
+                                                        v('p').html().
+                                                        replace('\t', '').replace('\n', '').
+                                                        replace('\"', '').replace('\'', ''))
                     lines.pop(-1)
                     content_dict[k]['Title'] = '\n'.join(lines)
                     content_dict[k]['Count'] = int(rec[0])
                     content_dict[k]['Keywords'] = ' '.join(keywords)
 
                 # 将content_dict写入数据库即可
-                cursor.execute('begin transaction')
-                for k, v in enumerate(content_dict):
-                    if 'Title' in content_dict[k].keys():
-                        cursor.execute(insert_sql.format(table_name, content_dict[k]['Title'], content_dict[k]['Date'],
-                                                         content_dict[k]['Keywords'], content_dict[k]['Count'],
-                                                         content_dict[k]['Summary']))
-
-                cursor.execute('commit')
-                print('Processing..{}, 第{}页,用时：{:5.2f}秒'.format('+'.join(keywords), page,
-                                                                (datetime.now() - start_page_time).seconds))
-            count += 1
+                try:
+                    cursor.execute('begin transaction')
+                    for k, v in enumerate(content_dict):
+                        if 'Title' in content_dict[k].keys():
+                            cursor.execute(
+                                insert_sql.format(table_name, content_dict[k]['Title'], content_dict[k]['Date'],
+                                                  content_dict[k]['Keywords'], content_dict[k]['Count'],
+                                                  content_dict[k]['Summary']))
+                except Exception as e:
+                    print(e)
+                    print(content_dict[k]['Summary'])
+                    cursor.execute('rollback')
+                finally:
+                    cursor.execute('commit')
+                    print('Processing..{}, 第{:>2d}页,用时：{:5.2f}秒'.format('+'.join(keywords), page,
+                                                                    (datetime.now() - start_page_time).seconds))
+                    count += 1
         except StopIteration as e:
             print('Generator is done:', e.value)
             minutes, secs = divmod((datetime.now() - start_time).seconds, 60)
