4c8bde8b67a3764d3cf03b35946515dc2fe03f8d
bnpysse <bnpysse@aliyun.com>
2020-06-15 17:54:33 +0800

    只能是采用旧的查询接口了。2020-06-15 17:45:52

--- frequency.py
+++ frequency.py
@@ -1,25 +1,34 @@
 base_url = 'http://data.people.com.cn.proxy.library.georgetown.edu/rmrb/s?qs='
-qs = {"cds": [{"fld": "dataTime.start", "cdr": "AND", "hlt": "false", "vlr": "AND", "qtp": "DEF", "val": "1993-01-01"},
-              {"fld": "dataTime.end", "cdr": "AND", "hlt": "false", "vlr": "AND", "qtp": "DEF", "val": "2020-06-15"},
-              {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马+枪支"},
-                                     {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马+枪支"},
-                                     {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马+枪支"},
-                                     {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马+枪支"}]},
-              {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"},
-                                     {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"},
-                                     {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"},
-                                     {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"}]},
-              {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"},
-                                     {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"},
-                                     {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"},
-                                     {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"}]},
-              {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"},
-                                     {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"},
-                                     {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"},
-                                     {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马"}]},
-              {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马+枪支+暴力"},
-                                     {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马+枪支+暴力"},
-                                     {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "奥巴马+枪支+暴力"},
-                                     {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR",
-                                      "val": "奥巴马+枪支+暴力"}]}], "obs": [{"fld": "dataTime", "drt": "ASC"}]}
-base_url_tail = '&tr=A&ss=1&pageNo=2&pageSize=50'
+
+# 两个词的情况
+qs_2 = {
+    "cds": [{"fld": "dataTime.start", "cdr": "AND", "hlt": "false", "vlr": "AND", "qtp": "DEF", "val": "1993-01-01"},
+            {"fld": "dataTime.end", "cdr": "AND", "hlt": "false", "vlr": "AND", "qtp": "DEF", "val": "2020-06-15"},
+            {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
+                                   {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
+                                   {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
+                                   {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"}]},
+            {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
+                                   {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
+                                   {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
+                                   {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"}]}],
+    "obs": [{"fld": "dataTime", "drt": "ASC"}]}
+# 三个词的查询串
+qs_3 = {
+    "cds": [{"fld": "dataTime.start", "cdr": "AND", "hlt": "false", "vlr": "AND", "qtp": "DEF", "val": "1993-01-01"},
+            {"fld": "dataTime.end", "cdr": "AND", "hlt": "false", "vlr": "AND", "qtp": "DEF", "val": "2020-06-15"},
+            {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
+                                   {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
+                                   {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
+                                   {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"}]},
+            {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
+                                   {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
+                                   {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
+                                   {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"}]},
+            {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "暴力"},
+                                   {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "暴力"},
+                                   {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "暴力"},
+                                   {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "暴力"}]}],
+    "obs": [{"fld": "dataTime", "drt": "ASC"}]}
+
+base_url_tail = '&tr=A&ss=1&pageNo=1&pageSize=20'
