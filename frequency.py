16f2ce3530b3d2f85a8d05d08f7b9ae7a3076596
bnpysse <bnpysse@aliyun.com>
2020-06-22 22:18:56 +0800

    可以记录下当前已经运行了多少个词组,以及完成的比例。2020-06-22 22:18:37

--- frequency.py
+++ frequency.py
@@ -13,7 +13,7 @@ from pyquery import PyQuery as pq
 login_cookies_dict = {
     '_ga': 'GA1.2.576373095.1589077068',
     # 只需要改这个东东就可以，2020-06-11 17:14:42
-    'ezproxy': 'Q519XHzcZ4zXykW',
+    'ezproxy': '3mmkPwokhFJOYUz',
 }
 login_url = 'http://data.people.com.cn.proxy.library.georgetown.edu/rmrb/20200515/1?code=2'
 login_headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -28,44 +28,44 @@ login_s.get(login_url, headers=login_headers, cookies=cookies)
 date_start = '1993-01-01'
 date_end = '2020-06-15'
 pageSize = 50
-database_name = 'words_frequency.db'
-
+database_name = 'words_frequency_ABC.db'
+is_delete_last_words = True
 # region 查询串
 base_url = 'http://data.people.com.cn.proxy.library.georgetown.edu/rmrb/s?qs='
 
 # 两个词的情况
 # 访问的时候是：qs_2['cds'][2..3]['cds'][0..3]['val'],第一标为两个关键字，第二标为四种不同的变量
-qs_2 = {
-    "cds": [{"fld": "dataTime.start", "cdr": "AND", "hlt": "false", "vlr": "AND", "qtp": "DEF", "val": date_start},
-            {"fld": "dataTime.end", "cdr": "AND", "hlt": "false", "vlr": "AND", "qtp": "DEF", "val": date_end},
-            {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
-                                   {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
-                                   {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
-                                   {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"}]},
-            {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
-                                   {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
-                                   {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
-                                   {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"}]}],
-    "obs": [{"fld": "dataTime", "drt": "ASC"}]}
+qs_2={
+"cds":[{"fld":"dataTime.start","cdr":"AND","hlt":"false","vlr":"AND","qtp":"DEF","val":date_start},
+{"fld":"dataTime.end","cdr":"AND","hlt":"false","vlr":"AND","qtp":"DEF","val":date_end},
+{"cdr":"AND","cds":[{"fld":"title","cdr":"OR","hlt":"true","vlr":"OR","val":"克林顿"},
+{"fld":"subTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"克林顿"},
+{"fld":"introTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"克林顿"},
+{"fld":"contentText","cdr":"OR","hlt":"true","vlr":"OR","val":"克林顿"}]},
+{"cdr":"AND","cds":[{"fld":"title","cdr":"OR","hlt":"true","vlr":"OR","val":"枪支"},
+{"fld":"subTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"枪支"},
+{"fld":"introTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"枪支"},
+{"fld":"contentText","cdr":"OR","hlt":"true","vlr":"OR","val":"枪支"}]}],
+"obs":[{"fld":"dataTime","drt":"ASC"}]}
 
-# 三个词的查询串
-# 访问的时候是：qs_3['cds'][2..4]['cds'][0..3]['val'],第一标为三个关键字，第二标为四种不同的变量
-qs_3 = {
-    "cds": [{"fld": "dataTime.start", "cdr": "AND", "hlt": "false", "vlr": "AND", "qtp": "DEF", "val": date_start},
-            {"fld": "dataTime.end", "cdr": "AND", "hlt": "false", "vlr": "AND", "qtp": "DEF", "val": date_end},
-            {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
-                                   {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
-                                   {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"},
-                                   {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "克林顿"}]},
-            {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
-                                   {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
-                                   {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"},
-                                   {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "枪支"}]},
-            {"cdr": "AND", "cds": [{"fld": "title", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "暴力"},
-                                   {"fld": "subTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "暴力"},
-                                   {"fld": "introTitle", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "暴力"},
-                                   {"fld": "contentText", "cdr": "OR", "hlt": "true", "vlr": "OR", "val": "暴力"}]}],
-    "obs": [{"fld": "dataTime", "drt": "ASC"}]}
+#三个词的查询串
+#访问的时候是：qs_3['cds'][2..4]['cds'][0..3]['val'],第一标为三个关键字，第二标为四种不同的变量
+qs_3={
+"cds":[{"fld":"dataTime.start","cdr":"AND","hlt":"false","vlr":"AND","qtp":"DEF","val":date_start},
+{"fld":"dataTime.end","cdr":"AND","hlt":"false","vlr":"AND","qtp":"DEF","val":date_end},
+{"cdr":"AND","cds":[{"fld":"title","cdr":"OR","hlt":"true","vlr":"OR","val":"克林顿"},
+{"fld":"subTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"克林顿"},
+{"fld":"introTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"克林顿"},
+{"fld":"contentText","cdr":"OR","hlt":"true","vlr":"OR","val":"克林顿"}]},
+{"cdr":"AND","cds":[{"fld":"title","cdr":"OR","hlt":"true","vlr":"OR","val":"枪支"},
+{"fld":"subTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"枪支"},
+{"fld":"introTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"枪支"},
+{"fld":"contentText","cdr":"OR","hlt":"true","vlr":"OR","val":"枪支"}]},
+{"cdr":"AND","cds":[{"fld":"title","cdr":"OR","hlt":"true","vlr":"OR","val":"暴力"},
+{"fld":"subTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"暴力"},
+{"fld":"introTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"暴力"},
+{"fld":"contentText","cdr":"OR","hlt":"true","vlr":"OR","val":"暴力"}]}],
+"obs":[{"fld":"dataTime","drt":"ASC"}]}
 
 base_url_tail = '&tr=A&ss=1&pageNo={}&pageSize={}'
 
@@ -80,32 +80,34 @@ def read_word_list(filename):
         b = [line.rstrip() for line in lines[1].split(',')]
         c = [line.rstrip() for line in lines[2].split(',')]
     return a, b, c
-
-
 # endregion
 
 # region 生成组合词组,如果在数据库中有最末词组，则更改生成词组的列表
 def building_qs(last_words, *args):
     combination_list = list(itertools.product(*args))
+    # 词组的总量
+    total_words = len(combination_list)
+    reminder_words = 1
     if last_words is not None:
         pos = combination_list.index(tuple(last_words.split(' ')))
+        pos = pos if is_delete_last_words else pos + 1
         combination_list = combination_list[pos:]
+        # 如果词组发生了变化，那么对剩余词组数目重新赋值
+        reminder_words = pos
     for pos, word in enumerate(combination_list):
         if len(args) == 2:
             for i in range(4):
                 qs_2['cds'][2]['cds'][i]['val'] = word[0]
                 qs_2['cds'][3]['cds'][i]['val'] = word[1]
-            yield ([pos + 1, len(combination_list)]), word, \
+            yield ([pos + 1, total_words - reminder_words - pos - 1, total_words]), word, \
                   base_url + urllib.parse.quote(str(qs_2).replace('\'', '\"')).replace(' ', '') + base_url_tail
         elif len(args) == 3:
             for i in range(4):
                 qs_3['cds'][2]['cds'][i]['val'] = word[0]
                 qs_3['cds'][3]['cds'][i]['val'] = word[1]
                 qs_3['cds'][4]['cds'][i]['val'] = word[2]
-            yield ([pos + 1, len(combination_list)]), word, \
+            yield ([pos + 1, total_words - reminder_words - pos - 1, total_words]), word, \
                   base_url + urllib.parse.quote(str(qs_3).replace('\'', '\"')).replace(' ', '') + base_url_tail
-
-
 # endregion
 
 # region 从数据库中获取最末一个词组,有一种特殊情况，即这个词组已经完成，你重新运行程序的话，会全部删掉已经完成的词组，
@@ -125,8 +127,10 @@ def get_last_words(table_name):
         return None
     else:
         # 处理掉数据库中上次出错的那些词组记录，同时要更改自动生成的变量，即 sqlite_sequence 表中 seq 的值
+        # 如果不删除的话，即最末的这个词组没问题，那么不删除，应该把 last_words 赋成下一个词组
         last_words = rec['MyWords']
-        cur.execute('delete from `{}` where MyWords="{}";'.format(table_name, last_words))
+        if is_delete_last_words:
+            cur.execute('delete from `{}` where MyWords="{}";'.format(table_name, last_words))
         # 获取删除记录后的最末记录的ID，并更新 sqlite_sequence 中的seq值
         rec = cur.execute(sql_get_last_rec.format(table_name)).fetchone()
         id = rec['ID'] if rec is not None else 0
@@ -154,7 +158,9 @@ def get_word_frequency(table_name, *args):
         try:
             scale_word, my_words, url = next(gen)
             page = 1
-            print(scale_word[0], '/', scale_word[1], my_words, url.format(page, pageSize))
+            # print(scale_word[0], '/', scale_word[1], scale_word[2],  my_words, url.format(page, pageSize))
+            print(f'{scale_word[0]}/{scale_word[1]}, 总数:{scale_word[2]},'
+                  f'已完成率：{(scale_word[2]-scale_word[1])/scale_word[2]:4.2%}, {my_words}, {url.format(page, pageSize)}')
             # 首先取出第1页的内容，然后获得总的页数，再取剩余的页的内容
             response = login_s.get(url.format(page, pageSize), headers=login_headers,
                                    cookies=cookies).content.decode('utf-8')
@@ -203,7 +209,7 @@ def get_word_frequency(table_name, *args):
                                                   content_dict[k]['Layout'], content_dict[k]['Count'],
                                                   content_dict[k]['Summary']))
                     cursor.execute('commit')
-                    print('Processing..{}, 第{:>2d}页/总{:>3d}页/总{:>4d}条,用时：{:5.2f}秒'.
+                    print('\tProcessing..{}, 第{:>2d}页/总{:>3d}页/总{:>4d}条,用时：{:5.2f}秒'.
                           format('+'.join(my_words), page,
                                  page_total, total_record_num,
                                  (datetime.now() - start_page_time).seconds))
@@ -211,7 +217,7 @@ def get_word_frequency(table_name, *args):
                     print(e, content_dict[k]['KeyWords'])
                     cursor.execute('rollback')
             minutes, secs = divmod((datetime.now() - start_time).seconds, 60)
-            print('\tProcessing..{}，总计时间：{:2d}分{:2d}秒'.format('+'.join(my_words), minutes, secs))
+            print('Processing..{}，总计时间：{:2d}分{:2d}秒'.format('+'.join(my_words), minutes, secs))
         except StopIteration as e:
             print('Generator is done:', e.value)
             minutes, secs = divmod((datetime.now() - start_time).seconds, 60)
@@ -233,6 +239,39 @@ def create_sqlite_db(table_name):
     conn.close()
 
 
+# 使用的格式是，如果字符串，那么就是单纯地统计在数据表中存在的个数
+# 如果是List，那么就生成新的 Product，然后在数据表中统计每个词组的数量 2020-06-20 18:11:25
+def get_rec_count(table_name, *args):
+    sql_total = 'select count(*) as total from `{}`;'
+    sql_words_total = 'select count(*) as total from `{}` where MyWords="{}";'
+    my_database = 'words_frequency_{}.db'.format(table_name)
+    conn = sqlite3.connect(my_database)
+    cur = conn.cursor()
+    cur.row_factory = sqlite3.Row
+    if args is None:
+        result = cur.execute(sql_total.format(table_name)).fetchone()
+        print('The total of {} is {}'.format(table_name, result['total']))
+    elif isinstance(args[0], str):
+        w = ''
+        for item in args:
+            w += item + ' '
+        w = w.replace('+', ' ').rstrip()
+        result = cur.execute(sql_words_total.format(table_name, w)).fetchone()
+        print('The total of {} is {}'.format(w, result['total']))
+    else:
+        combination_list = list(itertools.product(*args))
+        for k,v in enumerate(combination_list):
+            w = ' '.join([*v])
+            result = cur.execute(sql_words_total.format(table_name, w)).fetchone()
+            print('{:>5d}--{:<20}Record is {:>5d}'.format(k, w, result['total']))
+    cur.close()
+    conn.close()
+
+
 if __name__ == '__main__':
     A, B, C = read_word_list('.word_list.txt')
-    get_word_frequency('AB', A, B)
+    # 有些情况下，当知晓最后一个词组是完整的情况下，是不需要删除数据库记录的，不然的话，还需要重新读取。
+    # 尤其是出错的情况下，再次读取时，默认状态下是删除最后一组词不完整的情况，执行后仍未获取到相关页面，
+    # 这种情况下就需要在这里进行设置为 False,即不能再删除了。哈哈，2020-06-19 18:50:33
+    is_delete_last_words = True
+    get_word_frequency('ABC', A, B, C)
